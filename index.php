963ce5408f84b121d6bcef464b97e871d1ba403a
Pedro Marcelo <pedromarcelodesaalves@gmail.com>
2014-09-05 01:00:59 -0300

    Arquivo index do plugin

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Social Package
+Plugin URI: https://github.com/pedromarcelojava/Social-Package
+Description: Pacote de redes sociais
+Version: 1.0
+Author: Pedro Marcelo
+Author URI: https://github.com/pedromarcelojava/
+License: GPL v2
+*/
+
+define('PLUGIN_URL', plugin_dir_url(__FILE__));
+define('PLUGIN_DIR', plugin_dir_path(__FILE__));
+
+include(PLUGIN_DIR . 'youtube/index.php');
