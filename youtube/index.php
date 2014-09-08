1b4294bb04e93c693b5a65e34b1db26eda6f06bb
Pedro Marcelo <pedromarcelodesaalves@gmail.com>
2014-09-08 02:49:47 -0300

    Adicionando shortcode e widget ao Youtube

--- youtube/index.php
+++ youtube/index.php
@@ -3,3 +3,5 @@
 include(PLUGIN_DIR . 'youtube/lib/class.youtube.php');
 include(PLUGIN_DIR . 'youtube/class.youtubedao.php');
 include(PLUGIN_DIR . 'youtube/menu/admin.php');
+include(PLUGIN_DIR . 'youtube/shortcode.php');
+include(PLUGIN_DIR . 'youtube/widget.php');
