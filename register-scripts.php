3d5677f215d2a1852e616ea25d8f5b1449790241
Pedro Marcelo <pedromarcelodesaalves@gmail.com>
2014-09-08 02:48:40 -0300

    Registrando scripts CSS e JS do Youtube

--- register-scripts.php
+++ register-scripts.php
@@ -1,9 +1,37 @@
 <?php
 
+/*
+|------------------------------------------
+|	JAVASCRIPT
+|------------------------------------------
+*/
+
 wp_register_script(
 	'form-package',
 	PLUGIN_URL . 'js/form.min.js',
 	array('jquery'),
 	null,
 	true
+);
+
+wp_register_script(
+	'youtube-package',
+	PLUGIN_URL . 'youtube/js/script.js',
+	array('jquery'),
+	null,
+	true
+);
+
+/*
+|------------------------------------------
+|	CSS
+|------------------------------------------
+*/
+
+wp_register_style(
+	'youtube-package',
+	PLUGIN_URL . 'youtube/css/style.min.css',
+	array(),
+	null,
+	'screen'
 );
